436834bce6ca505bb60e0a980e74800ae23c4f30
Angelika <wunderange@rambler.ru>
2023-02-05 16:13:14 +0300

    add seminar 3 task 25_23

---
+++ Seminar_3_Task_25/Program.cs
@@ -0,0 +1,24 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+Console.Clear();
+Console.Write("Введите число N: ");
+int num = int.Parse(Console.ReadLine()!);
+int a = 1;
+while (a <= num)
+{
+  double cub = Math.Pow(a, 3);
+  Console.Write($"{cub}");
+  if (a != num)
+  {
+    Console.Write(", ");
+  }
+  else
+  {
+    Console.Write(".");
+  }
+  a++;
+}
+
+
